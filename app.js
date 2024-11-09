0e513c63ac442c0ce78b880dfc7b93b781f5c54b
Azeem Sardar <azeemsardar228@gmail.com>
2024-11-09 09:43:04 +0500

    Resolved merge conflicts

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var toggleSkillsButton = document.getElementById('toggle-skills-btn');
-var skillsList = document.getElementById('skills-list');
-toggleSkillsButton.addEventListener('click', function () {
-    if (skillsList.style.display === 'none' || !skillsList.style.display) {
-        skillsList.style.display = 'block';
+// const toggleSkillsButton = document.getElementById('toggle-skills-btn') as HTMLButtonElement;
+// const skillsList = document.getElementById('skills-list') as HTMLUListElement;
+// toggleSkillsButton.addEventListener('click', () => {
+//     if(skillsList.style.display === 'none' || !skillsList.style.display){
+//         skillsList.style.display = 'block';
+//     } else {
+//         skillsList.style.display = 'none';
+//     }
+// });
+var btn = document.querySelector(".btn");
+var skillsList = document.querySelector(".skills");
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
+    if (skillsList.style.display === "none" || !skillsList.style.display) {
+        skillsList.style.display = "block";
     }
     else {
-        skillsList.style.display = 'none';
+        skillsList.style.display = "none";
     }
 });
--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const toggleSkillsButton = document.getElementById('toggle-skills-btn') as HTMLButtonElement;
 const skillsList = document.getElementById('skills-list') as HTMLUListElement;
 
@@ -8,3 +9,30 @@ toggleSkillsButton.addEventListener('click', () => {
         skillsList.style.display = 'none';
     }
 });
+=======
+// const toggleSkillsButton = document.getElementById('toggle-skills-btn') as HTMLButtonElement;
+// const skillsList = document.getElementById('skills-list') as HTMLUListElement;
+
+// toggleSkillsButton.addEventListener('click', () => {
+//     if(skillsList.style.display === 'none' || !skillsList.style.display){
+//         skillsList.style.display = 'block';
+//     } else {
+//         skillsList.style.display = 'none';
+//     }
+// });
+
+
+
+
+let btn = document.querySelector(".btn") as HTMLButtonElement;
+let skillsList = document.querySelector(".skills") as HTMLElement;
+
+btn?.addEventListener("click", () =>{
+    if(skillsList.style.display === "none" || !skillsList.style.display){
+        skillsList.style.display = "block";
+    }
+    else{
+        skillsList.style.display = "none";
+    }
+});
+>>>>>>> 530cbe0 (resume updated)
