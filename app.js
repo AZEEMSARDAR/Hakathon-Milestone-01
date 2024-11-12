2aa7e06b7efe344b90ea5da84e5929096e364d06
Azeem Sardar <azeemsardar228@gmail.com>
2024-11-12 09:39:04 +0500

    updated new static resume

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-// const toggleSkillsButton = document.getElementById('toggle-skills-btn') as HTMLButtonElement;
-// const skillsList = document.getElementById('skills-list') as HTMLUListElement;
-// toggleSkillsButton.addEventListener('click', () => {
-//     if(skillsList.style.display === 'none' || !skillsList.style.display){
-//         skillsList.style.display = 'block';
-//     } else {
-//         skillsList.style.display = 'none';
-//     }
-// });
 var btn = document.querySelector(".btn");
-var skillsList = document.querySelector(".skills");
+var skillsList = document.querySelector("#skillsList");
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
     if (skillsList.style.display === "none" || !skillsList.style.display) {
         skillsList.style.display = "block";
--- app.ts
+++ app.ts
@@ -1,33 +1,7 @@
-<<<<<<< HEAD
-const toggleSkillsButton = document.getElementById('toggle-skills-btn') as HTMLButtonElement;
-const skillsList = document.getElementById('skills-list') as HTMLUListElement;
+const btn = document.querySelector(".btn") as HTMLButtonElement;
+const skillsList = document.querySelector("#skillsList") as HTMLElement;
 
-toggleSkillsButton.addEventListener('click', () => {
-    if(skillsList.style.display === 'none' || !skillsList.style.display){
-        skillsList.style.display = 'block';
-    } else {
-        skillsList.style.display = 'none';
-    }
-});
-=======
-// const toggleSkillsButton = document.getElementById('toggle-skills-btn') as HTMLButtonElement;
-// const skillsList = document.getElementById('skills-list') as HTMLUListElement;
-
-// toggleSkillsButton.addEventListener('click', () => {
-//     if(skillsList.style.display === 'none' || !skillsList.style.display){
-//         skillsList.style.display = 'block';
-//     } else {
-//         skillsList.style.display = 'none';
-//     }
-// });
-
-
-
-
-let btn = document.querySelector(".btn") as HTMLButtonElement;
-let skillsList = document.querySelector(".skills") as HTMLElement;
-
-btn?.addEventListener("click", () =>{
+btn?.addEventListener("click", () => {
     if(skillsList.style.display === "none" || !skillsList.style.display){
         skillsList.style.display = "block";
     }
@@ -35,4 +9,8 @@ btn?.addEventListener("click", () =>{
         skillsList.style.display = "none";
     }
 });
->>>>>>> 530cbe0 (resume updated)
+
+
+
+
+
